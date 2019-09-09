d161f1a0bc80d63ec711f69df9c07f1cead1dce7
Joshua Schoenfeld <jschoe04@gmail.com>
2019-09-09 09:04:59 -0400

    Creating a Service to Get Album Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-product-page></app-product-page>
+  `,
+  styles: ['']
 })
 export class AppComponent {
   title = 'app works!';
